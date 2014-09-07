444aab1b0fdac3cd9932249994317e573babadb9
Jhatzig <j.hatzig@gmail.com>
2014-09-07 19:31:50 +0200

    KataPotter
    Version n° 1.
    Je n'arrive pas a trouver l'algo pour récupérer le plus petit prix.

---
+++ KataPotter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataPotter
+{
+    public static class KataPotter
+    {
+
+        private static Dictionary<int, double> remises = new Dictionary<int, double>();
+
+        private static double defaultPriceBook = 8;
+
+        public static double getPrices(int[] books)
+        {
+            remises = remises.Count == 0 ? InitRemise() : remises;
+            return books.Count() != 0 ? getPricesBook(books) : 0;
+        }
+
+        private static double getPricesBook(int[] books)
+        {
+            if (books.Count() == 1)
+            {
+                return defaultPriceBook;
+            }
+            var price = 0.0;
+            double remise;
+            remises.TryGetValue(books.Distinct().Count(), out remise);
+
+            price = books.Distinct().Count() * defaultPriceBook * remise;
+            
+            var dif = books.Count() - books.Distinct().Count();
+            if (dif > 0)
+            {
+                price += dif * defaultPriceBook;
+            }
+
+            return price;
+
+        }
+
+
+        private static Dictionary<int, double> InitRemise()
+        {
+            remises.Add(1, 1);
+            remises.Add(2, 0.95);
+            remises.Add(3, 0.90);
+            remises.Add(4, 0.80);
+            remises.Add(5, 0.75);
+            return remises;
+        }
+
+    }
+
+}
---
+++ KataPotterTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace KataPotter
+{
+    [TestClass]
+    public class KataPotterTest
+    {
+
+        public int[] books;
+
+        [TestMethod]
+        public void HowMuchForOBook()
+        {
+            books = new int[] { };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 0);
+        }
+
+        [TestMethod]
+        public void HowMuchFor1Book()
+        {
+            books = new int[] { 1 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 8);
+        }
+
+        [TestMethod]
+        public void HowMuchFor2CopiesOfTheSameCollection()
+        {
+            books = new int[] { 0, 0 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 16);
+        }
+
+
+        [TestMethod]
+        public void HowMuchFor3CopiesOfTheSameCollection()
+        {
+            books = new int[] { 0, 0, 0 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 24);
+        }
+
+        [TestMethod]
+        public void HowMuchFor4CopiesOfTheSameCollection()
+        {
+            books = new int[] { 0, 0, 0, 0 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 32);
+        }
+
+        [TestMethod]
+        public void HowMuchFor5CopiesOfTheSameCollection()
+        {
+            books = new int[] { 0, 0, 0, 0, 0 };
+
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 40);
+        }
+
+        [TestMethod]
+        public void HowMuchForTwoCollection()
+        {
+            books = new int[] { 0, 1 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 15.2);
+        }
+
+        [TestMethod]
+        public void HowMuchForThreeCollection()
+        {
+            books = new int[] { 0, 1, 2 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(price, 21.6);
+        }
+
+        [TestMethod]
+        public void HowMuchForTwoCollectionAndForMultipleCopies()
+        {
+            books = new int[] { 0, 0, 1 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(23.2, price);
+        }
+
+        [TestMethod]
+        public void HowMuchForTwoCollectionAndForTwoCopies()
+        {
+            books = new int[] { 0, 0, 1, 1 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(31.2, price);
+        }
+
+        [TestMethod]
+        public void HowMuchForThreeCollectionAndForTwoCopies()
+        {
+            books = new int[] { 0, 0, 1, 1, 2, 2 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(45.6, price);
+        }
+
+        [TestMethod]
+        public void HowMuchForFourCollectionAndMultipleCopies()
+        {
+            books = new int[] { 0, 0, 1, 1, 2, 2, 3 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(49.6, price);
+        }
+
+        [TestMethod]
+        public void HowMuchForFiveCollectionAndForMultipleCopies()
+        {
+            books = new int[] { 0, 0, 1, 1, 2, 2, 3, 4 };
+            var price = KataPotter.getPrices(books);
+            Assert.AreEqual(51.2, price);
+        }
+
+    }
+}
